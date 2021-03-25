5a252256739dc35a9970ed96e7faffdb06755abd
brinda chanchad <brinda@brindas-MacBook-Pro.local>
2021-03-25 19:30:33 +0530

    final q1

--- insert.php
+++ insert.php
@@ -18,45 +18,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student detail</title>
+    <style>
+        *{
+            padding:0px;
+            margin:0px;
+        }
+        .container{
+            display:grid;
+            place-items:center;
+            height:97vh;
+        }
+        
+        body{
+            background-color:#60abfb;
+        }
+        .card{
+           background-color:#aee1fc;
+           padding:10px;
+           width:25%;
+       }
+       .f{
+           padding:10px;
+           font-size:20px;
+       }
+    </style>
 </head>
 <body>
-    <table>
-        <tr>
-            <td>Full Name:</td>
-            <td><?php echo $name;?></td>
-        </tr>
-        <tr>
-            <td>Email id:</td>
-            <td><?php echo $email;?></td>
-        </tr>
-        <tr>
-            <td>Date of Birth:</td>
-            <td><?php echo $dob;?></td>
-        </tr>
-        <tr>
-            <td>Address:</td>
-            <td><?php echo $address;?></td>
-        </tr>
-        <tr>
-            <td>City:</td>
-            <td><?php echo $city;?></td>
-        </tr>
-        <tr>
-            <td>Gender:</td>
-            <td><?php echo $gender;?></td>
-        </tr>
-        <tr>
-            <td>Hobby:</td>
-            <td>
+<div class="container">
+    <div class="card">
+        <div class="f">
+            <b>Full Name:</b>
+            <?php echo $name;?>
+        </div>
+        <div class="f">
+        <b>Email id:</b>
+            <?php echo $email;?>
+        </div>
+        <div class="f">
+        <b> Date of Birth:</b>
+            <?php echo $dob;?>
+        </div>
+        <div class="f">
+        <b>Address:</b>
+            <?php echo $address;?>
+        </div>
+        <div class="f">
+        <b>City:</b>
+            <?php echo $city;?>
+        </div>
+        <div class="f">
+        <b>Gender:</b>
+            <?php echo $gender;?>
+        </div>
+        <div class="f">
+        <b>Hobby:</b>
+            
                 <?php 
                     echo implode(" ",$hobby); 
                 ?>
-            </td>
-        </tr>
-        <tr>
-            <td>password:</td>
-            <td><?php echo $password;?></td>
-        </tr>
-    </table>
+        </div>   
+        <div class="f">
+        <b>password:</b>
+            <?php echo $password;?>
+        </div>
+        </div>
+    </div>
 </body>
 </html>
--- q1.php
+++ q1.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student regestration Form</title>
-<<<<<<< HEAD
     <style>
         *{
             padding:0px;
@@ -18,139 +17,110 @@
         }
         .bg{
             height: 100vh;
-            background-color:#957DAD;
+            background-color:#eccafa;
         }
        .card{
-           background-color:#FDE6CC;
+           background-color:#fafff2;
+           padding:10px;
+           width:25%;
+       }
+       .p{
+           text-align:center;
+           border-radius: 5px;
            padding:10px;
        }
+       .cent{
+            width:100%;
+            text-align:center;
+            font-size:x-large;
+            font-weight:bold;
+            margin:10% 0%;
+       }
+       select,input[type=text],input[type=date],input[type=email],input[type=password]{
+           width:85%;
+           height:25px;
+           border-radius:5px; 
+           border:1px solid gray;
+       }
+       input[type=radio]
+       {
+           width:8%;
+           border:1px solid gray;
+       }
+       input[type=checkbox]
+       {
+           width:15%;
+           border:1px solid gray;
+       }
+       textarea{
+        width:85%;
+        border-radius:5px;
+        border:1px solid gray;
+       }
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
     </style>
 </head>
 <body class="bg">
 <div class="container">
 <div class="card">
    <form action="insert.php" method="get">
+        <p class="cent">Student registration form</p>
+        <div class="p">
+            <input type="text" name="name" required placeholder="Enter your full name"/>
+        </div>
+        <div class="p">
+            <input type="email" name="email" required placeholder="Enter your Emailid"/>
+        </div>
+        <div class="p">
+            <input type="date" name="date" required placeholder="Birthdate"/>
+        </div>
+        <div class="p">
+            <textarea name="add" rows="5" cols="20" required placeholder="Enter your address here..."></textarea>
+        </div>
+        <div class="p">
+            <select name="city">
+                <option value="Surat">Surat</option>
+                <option value="Ahemdabad">Ahemdabad</option>
+                <option value="Dhule">Dhule</option>
+                <option value="Pune">Pune</option>
+            </select>
+        </div>
+    
+        <div class="p">
+            <input type="radio" name="gender" checked group="sgender" value="Female"/>Female
+            <input type="radio" name="gender" group="sgender" value="Male"/>Male
+            <input type="radio" name="gender" group="sgender" value="Not to Say"/>Not to Say
+        </div>
+        <div class="p">
+            <input type="checkbox" name="hobby[]" checked value="Sports"/>Sports
+            <input type="checkbox" name="hobby[]" value="Swimming"/>Swimming<br>
+            <input type="checkbox" name="hobby[]" value="Reading"/>Reading
+            <input type="checkbox" name="hobby[]" value="Travelling"/>Travelling<br>
+            <input type="checkbox" name="hobby[]" value="Photography"/>Photography    
+        </div>
+        <div class="p">
+            <input type="password" name="password" required placeholder="Enter your password"/>
+        </div>
+        <div class="p">
+            <input type="submit" name="btn" value="submit"/>
+        </div>
 
-            <caption>Student regestration form</caption>
-            
-            
-            
-                <label for="name">Full name</label>
-                <input type="text" name="name" placeholder="Enter your full name"/>
-            
-            
-                <label for="emailid">Email id</label>
-                <input type="email" name="email" placeholder="Enter your Emailid"/>
-            
-            
-                <label for="dateofbirth">Date of Birth</label>
-                <input type="date" name="date" placeholder="Birthdate"/>
-            
-            
-                <label for="address">Address</label>
-                <textarea name="add" rows="5" cols="20"></textarea>
-            
-            
-                <label for="city">City</label>
-                
-=======
-</head>
-<body>
-   <form action="#" method="post">
-        <table>
-        <tr>
-            <td colspan="2">
-            <caption>Student regestration form</caption>
-            </td>
-            </tr>
-            <tr>
-                <td><label for="name">Full name</label></td>
-                <td><input type="text" name="name" placeholder="Enter your full name"/></td>
-            </tr>
-            <tr>
-                <td><label for="emailid">Email id</label></td>
-                <td><input type="email" name="email" placeholder="Enter your Emailid"/></td>
-            </tr>
-            <tr>
-                <td><label for="dateofbirth">Date of Birth</label></td>
-                <td><input type="date" name="date" placeholder="Birthdate"/></td>
-            </tr>
-            <tr>
-                <td><label for="address">Address</label></td>
-                <td><textarea name="add" rows="5" cols="20"></textarea></td>
-            </tr>
-            <tr>
-                <td><label for="city">City</label></td>
-                <td>
->>>>>>> origin/main
-                    <select name="city">
-                        <option value="Surat">Surat</option>
-                        <option value="Ahemdabad">Ahemdabad</option>
-                        <option value="Dhule">Dhule</option>
-                        <option value="Pune">Pune</option>
-                    </select>
-<<<<<<< HEAD
-                
-            
-            
-                <label for="gender">Gender</label>
-                
-                    <input type="radio" name="gender" group="sgender" value="Female"/>Female
-                    <input type="radio" name="gender" group="sgender" value="Male"/>Male
-                    <input type="radio" name="gender" group="sgender" value="Not to Say"/>Not to Say
-                
-            
-            
-                <label for="hobbies">Hobbies</label>
-                
-                    <input type="checkbox" name="hobby[]" value="Sports"/>Sports
-                    <input type="checkbox" name="hobby[]" value="Swimming"/>Swimming<br>
-                    <input type="checkbox" name="hobby[]" value="Reading"/>Reading
-                    <input type="checkbox" name="hobby[]" value="Travelling"/>Travelling<br>
-                    <input type="checkbox" name="hobby[]" value="Photography"/>Photography
-                
-            
-            
-                <label for="password">Password</label>
-            
-                    <input type="password" name="password"/>
-                    <input type="submit" name="btn" value="submit"/>
    </form>
    </div>
    </div>
 </body>
 </html>
-=======
-                </td>
-            </tr>
-            <tr>
-                <td><label for="gender">Gender</label></td>
-                <td>
-                    <input type="radio" name="gender" group="sgender" value="Female"/>Female
-                    <input type="radio" name="gender" group="sgender" value="Male"/>Male
-                    <input type="radio" name="gender" group="sgender" value="Not to Say"/>Not to Say
-                </td>
-            </tr>
-            <tr>
-                <td><label for="Date of birth">Hobbies</label></td>
-                <td>
-                <input type="checkbox" name="hobby" value="Sports"/>Sports
-                <input type="checkbox" name="hobby" value="Swimming"/>Swimming<br>
-                <input type="checkbox" name="hobby" value="Reading"/>Reading
-                <input type="checkbox" name="hobby" value="Travelling"/>Travelling<br>
-                <input type="checkbox" name="hobby" value="Photography"/>Photography
-                </td>
-            </tr>
-            <tr>
-                <td><label for="Date of birth">Date of Birth</label></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td><label for="Date of birth">Date of Birth</label></td>
-                <td></td>
-            </tr>
-        </table>
-   </form>
-</body>
-</html>
->>>>>>> origin/main
