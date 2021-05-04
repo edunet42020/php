f2804a546ad1c53bca804de86870ca9bac93c59d
brinda chanchad <brinda@brindas-MacBook-Pro.local>
2021-05-04 18:15:40 +0530

    changes

--- third.php
+++ third.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student detail</title>
+    <title>Passing variables through a URL</title>
     <style>
         *{
             padding:0px;
